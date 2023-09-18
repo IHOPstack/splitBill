e6807dd1229d075d8255301b0beef4744400d4b7
Devin Beckwith <beckwithdevin@yahoo.com>
2023-09-18 12:35:35 -0700

    streamlined testing process

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from math import ceil
+from kivy.core.window import Window
 from kivy.app import App
 from kivy.metrics import dp
 from kivy.properties import StringProperty, BooleanProperty
@@ -17,11 +18,11 @@ class Inputs(GridLayout):
     textl = "Name"
     textr = "$0.00"
     lineList = []
-    tax = TextInput(hint_text = "tax", size_hint = (None, None), size = (500,50))
-    tip = TextInput(hint_text = "tip", size_hint = (None, None), size = (500,50))
+    tax = TextInput(hint_text = "tax", size_hint = (.5, None), height = 50)
+    tip = TextInput(hint_text = "tip", size_hint = (.5, None), height = 50)
     def addLine(self, x):
         for i in range(0, (2 * x)):
-            TI = TextInput(hint_text = self.textl if i%2 == 0 else self.textr, size_hint = (None, None), size = (500,50))
+            TI = TextInput(hint_text = self.textl if i%2 == 0 else self.textr, size_hint = (.5, None), height = 50)
             self.add_widget(TI, index = len(self.lineList))
             self.lineList.append(TI)
     def __init__(self, **kwargs):
@@ -29,6 +30,17 @@ class Inputs(GridLayout):
         self.addLine(5)
         self.add_widget(self.tax)
         self.add_widget(self.tip)
+    def fill4test(self):
+        alph = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "z","ab", "bb", "cb", "db", "eb", "bf", "gb", "hb", "ib", "jb", "kb", "lb", "mb", "nb", "ob", "pb", "qb", "rb", "sb", "tb", "ub", "vb", "wb", "xb", "zb"]
+        i = 0
+        for textBox in self.lineList:
+            if textBox.hint_text == "Name":
+                textBox.text = alph[i]
+            else:
+                textBox.text = str(i)
+            i += 1
+        self.tax.text = str(i)
+        self.tip.text = str(i)
         
 class FullScreen(BoxLayout):
     people = {}
@@ -53,24 +65,25 @@ class FullScreen(BoxLayout):
             people[person] = ceil((taxTipShare + price) * 100) / 100
         self.ids.scroller.add_widget(Results())
 
-class Results(BoxLayout):
+class Results(GridLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.orientation = "vertical"
-        self.height = self.minimum_height
+        self.cols = 1
+        self.size_hint = 1, None
+        self.bind(minimum_height = self.setter("height"))
         people = FullScreen.people
         total = 0
         for key in people:
             cost = people[key]
             name = str(key)
-            self.add_widget(Label(text = f"{name} owes ${cost}", size_hint = (None, None), size = (1000, 50)))
+            self.add_widget(Label(text = f"{name} owes ${cost}", size_hint = (1, None), height = 50))
             total += cost
-        self.add_widget(Label(text = f"Total amound paid is ${total}", size_hint = (None, None), size = (1000, 50)))
+        self.add_widget(Label(text = f"Total amound paid is ${total}", size_hint = (1, None), height = 50))
 
 
 
 
 class splitBillApp(App):
-    pass
+    Window.size = (350, 700)
 
 splitBillApp().run()
