a70d2b5b30304f6b45db4cd90861fb8fc72ca999
Devin Beckwith <beckwithdevin@yahoo.com>
2023-09-20 15:46:57 -0700

    partial photo input functionality added

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from math import ceil
+import cv2
+import pytesseract
+import re
 from kivy.core.window import Window
 from kivy.app import App
 from kivy.metrics import dp
@@ -13,18 +16,23 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 
-
 class Inputs(GridLayout):
     textl = "Name"
     textr = "$0.00"
     lineList = []
+    placement = 0
     tax = TextInput(hint_text = "tax", size_hint = (.5, None), height = 50)
     tip = TextInput(hint_text = "tip", size_hint = (.5, None), height = 50)
     def addLine(self, x):
-        for i in range(0, (2 * x)):
-            TI = TextInput(hint_text = self.textl if i%2 == 0 else self.textr, size_hint = (.5, None), height = 50)
-            self.add_widget(TI, index = len(self.lineList))
-            self.lineList.append(TI)
+        for i in range(0, (x)):
+            TIname = TextInput(hint_text = self.textl, size_hint = (.5, None), height = 50)
+            TImoney = TextInput(hint_text = self.textr, size_hint = (.5, None), height = 50)
+            self.add_widget(TIname, index = self.placement)
+            self.placement -= 1
+            self.add_widget(TImoney, index = self.placement)
+            self.placement -= 1
+            TItuple = (TIname, TImoney)
+            self.lineList.append(TItuple)
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.addLine(5)
@@ -33,14 +41,48 @@ class Inputs(GridLayout):
     def fill4test(self):
         alph = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "z","ab", "bb", "cb", "db", "eb", "bf", "gb", "hb", "ib", "jb", "kb", "lb", "mb", "nb", "ob", "pb", "qb", "rb", "sb", "tb", "ub", "vb", "wb", "xb", "zb"]
         i = 0
-        for textBox in self.lineList:
-            if textBox.hint_text == "Name":
-                textBox.text = alph[i]
-            else:
-                textBox.text = str(i)
+        for tuple in self.lineList:
+            tuple[0].text = alph[i]
+            tuple[1].text = str(i)
             i += 1
         self.tax.text = str(i)
         self.tip.text = str(i)
+    def PhotoInput(self):
+        image = cv2.imread('receipt2.jpeg')
+        text = pytesseract.image_to_string(image)
+        moneyList = re.findall("\d+\.\d{2}", text)
+        entries = {}
+        taxEntry = None
+        tipEntry = None
+        for price in moneyList:
+            for line in text.split("\n"):
+                if price in line:
+                    ind = line.index(price)
+                    item = line[0:ind]
+                    if item.strip() == "TOTAL":
+                        break
+                    if "TAX" in item:
+                        taxEntry = price
+                        break
+                    if "TIP" in item:
+                        tipEntry = price
+                        break                   
+                    entries[item] = price
+        self.addLine((len(entries) - 5))
+        count = 0
+        for key in entries:
+            if taxEntry != None:
+                self.tax.text = str(taxEntry)
+            if tipEntry != None:
+                self.tip.text = str(tipEntry)
+            tuple = self.lineList[count]
+            tuple[0].text = key
+            tuple[1].text = entries[key]
+            count += 1        
+        
+
+
+
         
 class FullScreen(BoxLayout):
     people = {}
